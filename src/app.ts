bd84f9743009b21c34fb75853553026f79ec81cb
dyong <likedyong@gmail.com>
2017-05-05 20:55:49 +0900

    Add some skeleton code
    This skeleton code definitely help others understand this project
    structure.

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+export default class App {
+    hello(): string {
+        return 'hello';
+    }
+}
---
+++ src/spec/app.spec.ts
@@ -0,0 +1,12 @@
+import App from '../app';
+import * as Chai from 'chai';
+const expect = Chai.expect;
+
+describe('App', () => {
+  describe('hello()', () => {
+    it('should result in hello', () => {
+      const app = new App();
+      expect(app.hello()).to.eq('hello');
+    });
+  });
+});
